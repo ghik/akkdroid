d4a8caf09ae63bcdccab292a01239f4a9ee67e30
Roman Janusz <roman@student.agh.edu.pl>
2013-06-16 15:34:18 +0200

    code style refactors

--- client/src/main/scala/com/akkdroid/client/Akktivity.scala
+++ client/src/main/scala/com/akkdroid/client/Akktivity.scala
@@ -2,7 +2,7 @@ package com.akkdroid.client
 
 import android.app.Activity
 import android.os.{Handler, Bundle}
-import akka.actor.{Props, ActorRef, ActorSystem}
+import akka.actor.{ActorSelection, Props, ActorRef, ActorSystem}
 import android.widget._
 import java.{lang => jl, util => ju}
 import android.content.Intent
@@ -16,7 +16,7 @@ class Akktivity extends Activity {
   private var serviceURL: String = null
   private var adapter: ArrayAdapter[String] = null
   private var localActor: ActorRef = null
-  private var serverActor: ActorRef = null
+  private var serverActor: ActorSelection = null
 
   private var messageTextView: TextView = null
   private var sendButton: Button = null
@@ -27,54 +27,54 @@ class Akktivity extends Activity {
     super.onCreate(savedInstanceState)
 
     setContentView(R.layout.main)
-    loadUI
+    loadUI()
 
     val items = new ju.ArrayList[String]
     adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
     messagesList.setAdapter(adapter)
 
-    initializeActors
+    initializeActors()
 
     sendButton.onClickAsync { _ =>
       implicit val sender = localActor // impersonate our local actor so it can receive responses from server
       serverActor ! messageTextView.getText.toString
     }
     settingsButton.onClick { _ =>
-      startActivity(new Intent(getBaseContext(), classOf[AkkdroidPreferences]))
+      startActivity(new Intent(getBaseContext, classOf[AkkdroidPreferences]))
     }
   }
 
-  override def onStart {
-    super.onStart
-    initializeActors
+  override def onStart() {
+    super.onStart()
+    initializeActors()
   }
 
-  override def onResume {
-    super.onResume
-    initializeActors
+  override def onResume() {
+    super.onResume()
+    initializeActors()
   }
 
-  override def onDestroy {
-    system.shutdown
+  override def onDestroy() {
+    system.shutdown()
 
-    super.onDestroy
+    super.onDestroy()
   }
 
-  private def loadUI {
+  private def loadUI() {
     sendButton = findViewById(R.id.sendButton).asInstanceOf[Button]
     messageTextView = findViewById(R.id.messageText).asInstanceOf[TextView]
     settingsButton = findViewById(R.id.settingsButton).asInstanceOf[Button]
     messagesList = findViewById(R.id.messagesList).asInstanceOf[ListView]
   }
 
-  private def loadServiceURL() : String = {
-    val pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext())
-    val ip:String = pref.getString("pref_ip", getString(R.string.pref_ip_value))
-    val port:String = pref.getString("pref_port", getString(R.string.pref_port_value))
-    return s"akka://server-system@$ip:$port/user/server-actor"
+  private def loadServiceURL(): String = {
+    val pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext)
+    val ip: String = pref.getString("pref_ip", getString(R.string.pref_ip_value))
+    val port: String = pref.getString("pref_port", getString(R.string.pref_port_value))
+    s"akka://server-system@$ip:$port/user/server-actor"
   }
 
-  private def initializeActors {
+  private def initializeActors() {
     if (system == null) {
       system = ActorSystem("mobile-system")
 
@@ -90,7 +90,7 @@ class Akktivity extends Activity {
     val newServiceURL = loadServiceURL()
     if (newServiceURL != serviceURL) {
       serviceURL = newServiceURL
-      serverActor = system.actorFor(serviceURL)
+      serverActor = system.actorSelection(serviceURL)
     }
   }
 }
